aedc96a121f4f9d23f60773bd8659587bff2b2a5
Lus Perdigao <luis_s_p@hotmail.com>
2011-11-30 16:14:08 +0000

    adicionados WSs photoWS

---
+++ PhasebookWSs/src/example/photoWS.java
@@ -0,0 +1,41 @@
+package example;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+import data.Photo;
+
+import eai.PhotoBeanRemote;
+
+
+@WebService(name = "photows", targetNamespace = "http://PhasebookWSs/photoWS")
+public class photoWS {
+	@EJB(mappedName = "PhotoBeanRemote/remote")
+	PhotoBeanRemote photoBeanRem;
+	
+	
+	@WebMethod
+	public int privNumberOfPhotos(String id){
+		return photoBeanRem.privNumberOfPhotos( Integer.parseInt(id));
+	}
+	@WebMethod	
+	public int pubNumberOfPhotos(String id){
+		return photoBeanRem.pubNumberOfPhotos(Integer.parseInt(id));
+	}
+	@WebMethod	
+	public List<Photo> getPhotos(String id,String isFriend){
+		return photoBeanRem.getPhotos( Integer.parseInt(id),Boolean.parseBoolean(isFriend));
+	}
+	@WebMethod	
+	public boolean setProfilePhoto(String id,String password,String idPhoto){
+		return photoBeanRem.setProfilePhoto( Integer.parseInt(id), password, Integer.parseInt(idPhoto));
+	}
+	@WebMethod	
+	public String getPhotoById(String idPhoto){
+		return photoBeanRem.getPhotoById(Integer.parseInt(idPhoto));
+	}
+
+}
